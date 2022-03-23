a2467b581904f0234109c0d261793fc6458e555b
srikanth-500appsgit config --global user.email srikanth.munikot@500apps.com <srikanth.munikoti@500apps.com>
2022-03-23 19:11:24 +0530

    committeed8

---
+++ vuefolder/hello-world/src/static.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        hello
+     <h2>message:{{message}}</h2>
+     <h2>name:{{name}}</h2>
+     <h2>number:{{number}}</h2>
+    </div>
+</template>
+<script>
+export default {
+        name:"SriKanth",
+    props :{
+ message:String,
+ name:String,
+ number:Number,
+    },
+methods:{
+   fun(){
+       alert("hi")
+   }
+}
+}
+</script>
