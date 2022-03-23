faa53e4da4f98b23c528511d54208a2f46bce0ce
srikanth-500appsgit config --global user.email srikanth.munikot@500apps.com <srikanth.munikoti@500apps.com>
2022-03-23 19:13:16 +0530

    commit8

---
+++ srikanth/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Srikanth</h1>
+  </div>
+</template>
+<script>
+import new from '../first.vue'
+export default {
+    name:"HelloWorld"
+components:{
+  new,
+}
+props:{
+  msg:String
+}
+
+}
+</script>
+
