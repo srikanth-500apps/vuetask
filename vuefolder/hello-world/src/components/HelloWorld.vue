e9450419c89030366ae1ff0f9896fc6c40bd2bcb
srikanth-500appsgit config --global user.email srikanth.munikot@500apps.com <srikanth.munikoti@500apps.com>
2022-03-23 19:08:23 +0530

    commit8

---
+++ vuefolder/hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+   
+  <stati :message="message" :name="name" ref="num" :number="number"></stati>
+  <stati :message="message" :name="name" ref="numb" :number="number"></stati>
+   <button @click="buttonClick()">submit</button>
+  </div>
+</template>
+
+<script>
+
+import stati from '../static.vue'
+
+export default {
+  name: 'HelloWorld',
+components:{  
+  stati,
+  
+},
+  data(){
+    return{
+      message:"hi",
+      name:"Naveen",
+      number:123,
+    }
+  },
+methods:{
+   buttonClick(){
+     this.$refs.num.fun();
+    
+       
+     }
+   }
+}
+</script>
